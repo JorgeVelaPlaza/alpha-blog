3f0ddab604494e9bd1e997b83dcba5e1d6a200f5
Jorge Vela <velaplazajorge@gmail.com>
2016-11-21 18:52:34 +0100

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
